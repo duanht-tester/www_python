97e5927cb3f7428b374e2b2c36554be84abc52b8
lianxi <46497378@qq.com>
2019-12-26 15:14:30 +0800

    1.01

---
+++ jcyf/blz.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+"""
+# 保留字
+import keyword as kw
+blz = kw.kwlist
+print(blz)
+"""
+
+# 注释
+# 第一个注释
+print("Hello, Python!")  # 第二个注释
+# 第一个注释
+# 第二个注释
+
+'''
+第三注释
+第四注释
+'''
+
+"""
+第五注释
+第六注释
+"""
+print("Hello, Python!")

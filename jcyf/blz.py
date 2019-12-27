01bf4f748c2e06728f4bf2453fcbb6d7315164de
lianxi <46497378@qq.com>
2019-12-27 18:02:53 +0800

    1.01

--- jcyf/blz.py
+++ jcyf/blz.py
@@ -47,7 +47,7 @@ total = ['item_one', 'item_two', 'item_three',
          'item_four', 'item_five']
 print(total)
 """
-
+"""
 # 字符串
 str= 'Runoob'
 print(str)
@@ -61,3 +61,38 @@ print(str + "你好！")
 print('--------------------')
 print("hello\nrunoob!")
 print(r"hello\nrunoob")
+"""
+"""
+# 等待用户输入
+input("\n\n按下enter键退出。")
+"""
+"""
+# 同一行显示多条语句
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+"""
+"""
+# Print 输出
+x = 'a'
+y = 'b'
+print(x)
+print(y)
+print('---------')
+print(x, end='')
+print(y, end='')
+"""
+"""
+# 导入 sys 模块
+import sys
+print('================Python import mode==========================')
+print ('命令行参数为:')
+for i in sys.argv:
+    print(i)
+print ('\n python 路径为', sys.path)
+from sys import argv,path
+print('================python from import===================================')
+print('path', path)
+"""
+
+
+
+

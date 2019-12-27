65d84c8a675f6114fc84ad7eca83b8f5c7ab25c5
lianxi <46497378@qq.com>
2019-12-27 09:28:18 +0800

    1.01

--- jcyf/blz.py
+++ jcyf/blz.py
@@ -6,7 +6,7 @@ import keyword as kw
 blz = kw.kwlist
 print(blz)
 """
-
+"""
 # 注释
 # 第一个注释
 print("Hello, Python!")  # 第二个注释
@@ -17,9 +17,47 @@ print("Hello, Python!")  # 第二个注释
 第三注释
 第四注释
 '''
-
-"""
+'''
 第五注释
 第六注释
-"""
+'''
 print("Hello, Python!")
+"""
+"""
+# 行与缩进
+if True:
+    print('True')
+else:
+    print('False')
+
+if True:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+print ("False")    # 缩进不一致，会导致运行错误
+"""
+"""
+# 多行语句
+total = "item_one + \
+    item_two +\
+    item_three"
+print(total)
+total = ['item_one', 'item_two', 'item_three',
+         'item_four', 'item_five']
+print(total)
+"""
+
+# 字符串
+str= 'Runoob'
+print(str)
+print(str[0:-1])
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str[:3])
+print(str * 2)
+print(str + "你好！")
+print('--------------------')
+print("hello\nrunoob!")
+print(r"hello\nrunoob")

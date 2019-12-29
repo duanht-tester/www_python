4a8483849b7a444c799b4769972f79fe4fdf78a1
runoob-git-test <46497378@qq.com>
2019-12-29 23:52:28 +0800

    1.01

--- jibenshujulx/num_type.py
+++ jibenshujulx/num_type.py
@@ -47,9 +47,42 @@ print(str[-1])
 print(str[-6])
 print(str[5])
 """
-
+"""
 print('Ru\noob')
 print(r'Ru\noob')
 word = 'Python'
 print(word[0], word[5])
 print(word[-1], word[-6])
+"""
+"""
+list = ['abcd', 786, 2.23, 'runoob', 70.2]
+tinylist = [123, 'runoob']
+print(list)
+print(list[0])
+print(list[1:3])
+print(list[2:])
+print(tinylist * 2)
+print(list + tinylist)
+
+a = [1, 2, 3, 4, 5, 6]
+a[0] = 9
+a[2:5] = [13, 14, 15]
+print(a)
+a[2:5] = []
+print(a)
+"""
+
+
+def reverseWords(input):
+
+    inputWords = input.split(" ")
+    inputWords = inputWords[-1::-1]
+    output = ' '.join(inputWords)
+    return output
+
+
+if __name__ == "__main__":
+    input = "I like runoob"
+    rw = reverseWords(input)
+    print(rw)
+

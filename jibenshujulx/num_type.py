871814098d776bc95206d69bf27007be0422531b
lianxi <46497378@qq.com>
2020-01-01 17:58:22 +0800

    1.01

--- jibenshujulx/num_type.py
+++ jibenshujulx/num_type.py
@@ -112,6 +112,33 @@ print(tuple[2:])         # 输出从第三个元素开始的所有元素
 print(tinytuple * 2)     # 输出两次元组
 print(tuple + tinytuple) # 连接元组
 """
-
+"""
 student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
-print(student)
+print(student)  # 输出集合，重复的元素被自动去掉
+# 成员测试
+if 'Rose' in student:
+    print('Rose 在集合中')
+else:
+    print('Rose 不在集合中')
+
+# set可以进行集合运算
+a = set('abracadabra')
+b = set('alacazam')
+print(a)
+print(a - b)  # a 和 b 的差集
+print(a | b)  # a 和 b 的并集
+print(a & b)  # a 和 b 的交集
+print(a ^ b)  # a 和 b 中不同时存在的元素
+"""
+
+# 字典
+dict = {}
+dict['one'] = "1 - 菜鸟教程"
+dict[2] = "2 - 菜鸟工具"
+tinydict = {'name': 'runoob', 'code': 1, 'site': 'www.runoob.com'}
+
+print(dict['one'])  # 输出键为 'one' 的值
+print(dict[2])  # 输出键为 2 的值
+print(tinydict)  # 输出完整的字典
+print(tinydict.keys())  # 输出所有键
+print(tinydict.values())  # 输出所有值
---
+++ jibenshujulx/yunsuanfu.py
@@ -0,0 +1,115 @@
+
+"""
+# 计算运算符
+a = 21
+b = 10
+c = 0
+c = a + b
+print("1 - c 的值为：", c)
+c = a - b
+print("2 - c 的值为：", c)
+c = a * b
+print("3 - c 的值为：", c)
+c = a / b
+print("4 - c 的值为：", c)
+c = a % b
+print("5 - c 的值为：", c)
+
+# 修改变量 a 、b 、c
+a = 2
+b = 3
+c = a ** b
+print("6 - c 的值为：", c)
+
+a = 10
+b = 5
+c = a // b
+print("7 - c 的值为：", c)
+"""
+"""
+# 比较运算符
+a = 21
+b = 10
+c = 0
+
+if (a == b):
+    print("1 - a 等于 b")
+else:
+    print("1 - a 不等于 b")
+
+if (a != b):
+    print("2 - a 不等于 b")
+else:
+    print("2 - a 等于 b")
+
+if (a < b):
+    print("3 - a 小于 b")
+else:
+    print("3 - a 大于等于 b")
+
+if (a > b):
+    print("4 - a 大于 b")
+else:
+    print("4 - a 小于等于 b")
+
+# 修改变量 a 和 b 的值
+a = 5
+b = 20
+if (a <= b):
+    print("5 - a 小于等于 b")
+else:
+    print("5 - a 大于  b")
+
+if (b >= a):
+    print("6 - b 大于等于 a")
+else:
+    print("6 - b 小于 a")
+"""
+"""
+# 赋值运算符
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print("1 - c 的值为：", c)
+c += a
+print("2 - c 的值为：", c)
+c *= a
+print("3 - c 的值为：", c)
+c /= a
+print("4 - c 的值为：", c)
+c = 2
+c %= a
+print("5 - c 的值为：", c)
+c **= a
+print("6 - c 的值为：", c)
+c //= a
+print("7 - c 的值为：", c)
+"""
+"""
+# 位运算符
+a = 60  # 60 = 0011 1100
+b = 13  # 13 = 0000 1101
+c = 0
+
+c = a & b;  # 12 = 0000 1100
+print("1 - c 的值为：", c)
+
+c = a | b;  # 61 = 0011 1101
+print("2 - c 的值为：", c)
+
+c = a ^ b;  # 49 = 0011 0001
+print("3 - c 的值为：", c)
+
+c = ~a;  # -61 = 1100 0011
+print("4 - c 的值为：", c)
+
+c = a << 2;  # 240 = 1111 0000
+print("5 - c 的值为：", c)
+
+c = a >> 2;  # 15 = 0000 1111
+print("6 - c 的值为：", c)
+"""
+
+
